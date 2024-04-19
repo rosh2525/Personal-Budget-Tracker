dfe4ff36025dc640f3043cbfd1d4296b4975b33d
Roshan. K <b23bb1037@iitj.ac.in>
2024-04-19 07:05:50 +0530

    can now save expenses in a csv file

--- gui.c
+++ gui.c
@@ -1,13 +1,16 @@
 #include "gui.h"
 #include <ctype.h>
 #include <time.h>
+#define CSV_FILE "expenses.csv"
 
 GtkWidget *expense_entry;
 GtkWidget *category_combo;
 GtkWidget *expense_scrolled_window;
 GtkWidget *delete_button;
 GtkListStore *store;
-const gchar *categories[] = {"Groceries", "Transportation", "Entertainment", "Utilities"};
+GtkWidget *sort_button;
+
+const gchar *categories[] = {"Groceries", "Transportation", "Entertainment", "Utilities", "Others"};
 
 GtkWidget *create_main_window() {
     GtkWidget *window;
@@ -17,20 +20,25 @@ GtkWidget *create_main_window() {
     GtkWidget *clear_button;
     GtkWidget *tree_view;
 
+    // Create the main window
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "Personal Expense Tracker");
-    gtk_window_set_default_size(GTK_WINDOW(window), 400, 300);
+    gtk_window_set_title(GTK_WINDOW(window), "Personal Budget Tracker");
+    gtk_window_set_default_size(GTK_WINDOW(window), 400, 500);
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
+    // Create a main vertical box
     main_box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
     gtk_container_add(GTK_CONTAINER(window), main_box);
 
+    // Create an expense entry field
     expense_entry = gtk_entry_new();
     gtk_entry_set_placeholder_text(GTK_ENTRY(expense_entry), "Enter expense amount");
     gtk_box_pack_start(GTK_BOX(main_box), expense_entry, FALSE, FALSE, 0);
 
+    // Connect signal handler for entry field
     g_signal_connect(expense_entry, "changed", G_CALLBACK(on_entry_changed), NULL);
 
+    // Create a combo box for expense categories
     category_combo = gtk_combo_box_text_new();
     for (int i = 0; i < G_N_ELEMENTS(categories); ++i) {
         gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(category_combo), categories[i]);
@@ -38,49 +46,64 @@ GtkWidget *create_main_window() {
     gtk_combo_box_set_active(GTK_COMBO_BOX(category_combo), 0);
     gtk_box_pack_start(GTK_BOX(main_box), category_combo, FALSE, FALSE, 0);
 
+    // Create a row to hold buttons
     GtkWidget *row = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 5);
     gtk_box_pack_start(GTK_BOX(main_box), row, FALSE, FALSE, 0);
 
+    // Create buttons
     delete_button = gtk_button_new_with_label("Delete");
-    gtk_box_pack_start(GTK_BOX(row), delete_button, FALSE, FALSE, 0);
-
     add_button = gtk_button_new_with_label("Add Expense");
-    gtk_box_pack_start(GTK_BOX(row), add_button, FALSE, FALSE, 0);
-
     clear_button = gtk_button_new_with_label("Clear All");
-    gtk_box_pack_start(GTK_BOX(row), clear_button, FALSE, FALSE, 0);
+    sort_button = gtk_button_new_with_label("Sort by Category");
 
+    // Pack buttons into the row
+    gtk_box_pack_start(GTK_BOX(row), delete_button, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(row), add_button, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(row), clear_button, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(row), sort_button, FALSE, FALSE, 0);
+
+    // Create a label for the expense list
     label = gtk_label_new("Expense List:");
     gtk_box_pack_start(GTK_BOX(main_box), label, FALSE, FALSE, 0);
 
+    // Create a scrolled window for the tree view
     expense_scrolled_window = gtk_scrolled_window_new(NULL, NULL);
     gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(expense_scrolled_window), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
     gtk_box_pack_start(GTK_BOX(main_box), expense_scrolled_window, TRUE, TRUE, 0);
 
-    store = gtk_list_store_new(3, G_TYPE_STRING, G_TYPE_STRING, G_TYPE_STRING);
+    // Create the list store
+    store = gtk_list_store_new(NUM_COLUMNS, G_TYPE_STRING, G_TYPE_STRING, G_TYPE_STRING);
 
+    // Create the tree view
     tree_view = gtk_tree_view_new_with_model(GTK_TREE_MODEL(store));
     gtk_container_add(GTK_CONTAINER(expense_scrolled_window), tree_view);
 
+    // Add columns to the tree view
     GtkCellRenderer *renderer;
     GtkTreeViewColumn *column;
 
     renderer = gtk_cell_renderer_text_new();
-    column = gtk_tree_view_column_new_with_attributes("Price", renderer, "text", 0, NULL);
+    column = gtk_tree_view_column_new_with_attributes("Price", renderer, "text", COLUMN_PRICE, NULL);
     gtk_tree_view_append_column(GTK_TREE_VIEW(tree_view), column);
 
     renderer = gtk_cell_renderer_text_new();
-    column = gtk_tree_view_column_new_with_attributes("Time", renderer, "text", 1, NULL);
+    column = gtk_tree_view_column_new_with_attributes("Time", renderer, "text", COLUMN_TIME, NULL);
     gtk_tree_view_append_column(GTK_TREE_VIEW(tree_view), column);
 
     renderer = gtk_cell_renderer_text_new();
-    column = gtk_tree_view_column_new_with_attributes("Reason", renderer, "text", 2, NULL);
+    column = gtk_tree_view_column_new_with_attributes("Reason", renderer, "text", COLUMN_REASON, NULL);
     gtk_tree_view_append_column(GTK_TREE_VIEW(tree_view), column);
 
+    // Load expenses from CSV file
+    load_expenses_from_csv();
+
+    // Connect signal handlers
     g_signal_connect(delete_button, "clicked", G_CALLBACK(on_delete_clicked), tree_view);
     g_signal_connect(add_button, "clicked", G_CALLBACK(on_add_expense_clicked), NULL);
     g_signal_connect(clear_button, "clicked", G_CALLBACK(on_clear_all_clicked), tree_view);
+    g_signal_connect(sort_button, "clicked", G_CALLBACK(on_sort_by_category_clicked), tree_view);
 
+    // Show all widgets
     gtk_widget_show_all(window);
 
     return window;
@@ -104,6 +127,8 @@ void on_add_expense_clicked(GtkButton *button, gpointer data) {
 
         g_free((gchar *) category_text);
     }
+
+    save_expenses_to_csv();
 }
 
 void on_entry_changed(GtkEditable *editable, gpointer user_data) {
@@ -129,10 +154,58 @@ void on_delete_clicked(GtkButton *button, gpointer data) {
     if (gtk_tree_selection_get_selected(selection, &model, &iter)) {
         gtk_list_store_remove(GTK_LIST_STORE(model), &iter);
     }
+
+    save_expenses_to_csv();
 }
 
 void on_clear_all_clicked(GtkButton *button, gpointer data) {
     GtkTreeModel *model = gtk_tree_view_get_model(GTK_TREE_VIEW(data));
     gtk_list_store_clear(GTK_LIST_STORE(model));
+
+    save_expenses_to_csv();
 }
+
+void load_expenses_from_csv() {
+    FILE *file = fopen(CSV_FILE, "r");
+    if (file) {
+        char line[256];
+        while (fgets(line, sizeof(line), file)) {
+            char *price, *time, *reason;
+            price = strtok(line, ",");
+            time = strtok(NULL, ",");
+            reason = strtok(NULL, ",");
+            if (price && time && reason) {
+                GtkTreeIter iter;
+                gtk_list_store_append(store, &iter);
+                gtk_list_store_set(store, &iter, COLUMN_PRICE, price, COLUMN_TIME, time, COLUMN_REASON, reason, -1);
+            }
+        }
+        fclose(file);
+    }
+}
+
+void save_expenses_to_csv() {
+    FILE *file = fopen(CSV_FILE, "w");
+    if (file) {
+        GtkTreeIter iter;
+        gboolean valid = gtk_tree_model_get_iter_first(GTK_TREE_MODEL(store), &iter);
+        while (valid) {
+            gchar *price, *time, *reason;
+            gtk_tree_model_get(GTK_TREE_MODEL(store), &iter, COLUMN_PRICE, &price, COLUMN_TIME, &time, COLUMN_REASON, &reason, -1);
+            fprintf(file, "%s,%s,%s\n", price, time, reason);
+            g_free(price);
+            g_free(time);
+            g_free(reason);
+            valid = gtk_tree_model_iter_next(GTK_TREE_MODEL(store), &iter);
+        }
+        fclose(file);
+    }
+}
+
+void on_sort_by_category_clicked(GtkButton *button, gpointer data) {
+    GtkTreeView *tree_view = GTK_TREE_VIEW(data);
+    GtkTreeSortable *sortable = GTK_TREE_SORTABLE(gtk_tree_view_get_model(GTK_TREE_VIEW(tree_view)));
+    gtk_tree_sortable_set_sort_column_id(sortable, COLUMN_REASON, GTK_SORT_ASCENDING);
+}
+
 
--- gui.h
+++ gui.h
@@ -2,15 +2,30 @@
 #define GUI_H
 
 #include <gtk/gtk.h>
+#include <ctype.h>
+#include <time.h>
 
+// Define CSV file name
+#define CSV_FILE "expenses.csv"
+
+// Define column indices
+enum {
+    COLUMN_PRICE,
+    COLUMN_TIME,
+    COLUMN_REASON,
+    NUM_COLUMNS
+};
+
+// Function prototypes
 GtkWidget *create_main_window();
-
 void on_add_expense_clicked(GtkButton *button, gpointer data);
-
-void on_delete_clicked(GtkButton *button, gpointer data);
-
-void on_clear_all_clicked(GtkButton *button, gpointer data);
-
 void on_entry_changed(GtkEditable *editable, gpointer user_data);
+void on_delete_clicked(GtkButton *button, gpointer data);
+void on_clear_all_clicked(GtkButton *button, gpointer data);
+void load_expenses_from_csv();
+void save_expenses_to_csv();
+void on_category_changed(GtkComboBox *widget, gpointer user_data);
+void on_dialog_response(GtkDialog *dialog, gint response_id, gpointer user_data);
+void on_sort_by_category_clicked(GtkButton *button, gpointer data);
 
-#endif
+#endif /* GUI_H */
